4bc14e99181b74a976418aa5830f88ae9e33d9aa
Kalv Sandhu <kalv@kalv.co.uk>
2018-08-27 21:54:50 -0700

    Adding quick tmux key

--- keyboards/atreus62/keymaps/kalv/keymap.c
+++ keyboards/atreus62/keymaps/kalv/keymap.c
@@ -12,7 +12,7 @@
 #define _RESET 2
 #define _LOWER 3
 
-#define ALT_TAB LGUI(KC_TAB)
+#define TMUX LCTL(KC_A)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_DEFAULT] = LAYOUT( /* qwerty */
@@ -20,7 +20,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC ,
     KC_ESC,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT ,
     KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                        KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,
-    KC_LCTL,  KC_LALT, KC_LGUI, ALT_TAB,  KC_LGUI, KC_BSPC, MO(_RAISE), MO(_LOWER),   KC_SPC,  KC_LEFT,  KC_DOWN, KC_UP, KC_RGHT,  KC_RGUI
+    KC_LCTL,  KC_LALT, KC_LGUI, TMUX,  KC_LGUI, KC_BSPC, MO(_RAISE), MO(_LOWER),   KC_SPC,  KC_LEFT,  KC_DOWN, KC_UP, KC_RGHT,  KC_RGUI
   ),
 
   [_RAISE] = LAYOUT(
