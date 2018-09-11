3e3c0643c3047844c0a12c688ac19b5b579294d6
Kalv Sandhu <kalv@kalv.co.uk>
2018-09-11 07:57:35 -0700

    Further improve my layout

--- keyboards/atreus62/keymaps/kalv/keymap.c
+++ keyboards/atreus62/keymaps/kalv/keymap.c
@@ -20,15 +20,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC ,
     KC_ESC,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT ,
     KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                        KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,
-    KC_LCTL,  KC_LALT, KC_LGUI, TMUX,  KC_LGUI, KC_BSPC, MO(_RAISE), MO(_LOWER),   KC_SPC,  KC_LEFT,  KC_DOWN, KC_UP, KC_RGHT,  KC_RGUI
+    KC_LCTL,  TMUX, KC_LALT, KC_LGUI, KC_LGUI, MO(_RAISE), KC_UP, MO(_LOWER),   KC_SPC,  KC_RGUI, KC_LEFT,  KC_DOWN, KC_UP, KC_RGHT
   ),
 
   [_RAISE] = LAYOUT(
-    TO(_DEFAULT),  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                       KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11  ,
-    KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                     KC_F12,  KC_TRNS, KC_TRNS,   KC_LCBR, KC_RCBR, KC_DEL ,
-    KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                     KC_TRNS, KC_PLUS, KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS,
+    TO(_RESET),  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                       KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11  ,
+    KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                     KC_F12,  KC_TRNS, KC_TRNS,   KC_LBRC, KC_RBRC, KC_DEL ,
+    KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                     KC_TRNS, KC_PLUS, KC_EQL, KC_LCBR, KC_RCBR, KC_BSLS,
     TO(_RESET),    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PIPE ,
-    KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+    KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS, KC_MPLY, KC_VOLD, KC_VOLU, KC_MNXT
   ),
 
   [_RESET] = LAYOUT(
